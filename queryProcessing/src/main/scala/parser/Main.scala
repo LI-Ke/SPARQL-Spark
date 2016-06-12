4461c437d6beabc69199b2d98d9842297d4b2968
oliv <oliver.cure@gmail.com>
2016-06-12 18:46:09 +0200

    Query modification

--- queryProcessing/src/main/scala/parser/Main.scala
+++ queryProcessing/src/main/scala/parser/Main.scala
@@ -3,7 +3,7 @@ package parser
 object Main extends App {
   println("start")
   //val qr = "SELECT ?x ?z1 WHERE {<http://www.dbpedia.org/resource/Clotrimazole> <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?x. ?x <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?y. ?x <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?z. ?x <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?z1. ?y <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?z. ?y <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?z2. ?y <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?z3.}"
-  val qr = "SELECT ?x ?z WHERE {<http://www.dbpedia.org/resource/Clotrimazole> <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?x. <http://www.dbpedia.org/resource/Clotrimazole> <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/atcCode> \"D01AC01\". ?z <http://www.w3.org/2002/07/owl#sameAs> <http://www.dbpedia.org/resource/Clotrimazole>}"
+  val qr = "SELECT ?x ?z WHERE {<http://www.dbpedia.org/resource/Clotrimazole> <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/brandName> ?x. <http://www.dbpedia.org/resource/Clotrimazole> <http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/atcCode> ?z.}"
 
   val directory = "/home/oliv/git/SPARQL-Spark/liteMat++/drugbankExt/"
 
@@ -17,5 +17,7 @@ object Main extends App {
   val lp = OptimizedPlan(collector).createLogicalPlan
 
 //  val pp = QueryPhysicalPlan(lp)
+
+  println(s"Query  = $qr")
   println("end")
 }
