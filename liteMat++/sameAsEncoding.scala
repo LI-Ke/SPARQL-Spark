87ec51d1be411ee51c19d3a042b4a016dddba2f0
oliv <oliver.cure@gmail.com>
2016-10-04 19:15:30 +0200

    new encoding (ccId, localId) scheme

--- liteMat++/sameAsEncoding.scala
+++ liteMat++/sameAsEncoding.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.graphx.Edge 
 import org.apache.spark.graphx.Graph
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Dataset
 
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.HashPartitioner
@@ -15,10 +16,21 @@ val file = "drugbankExt.nt"
 val triples0 = sc.textFile(directory+file).map(x=>x.split(" ")).map(t=>(t(0),t(1),t(2)))
 
 // create an RDD made of the owl:sameAs statements
-val sameAs = triples0.filter{case(s,p,o)=>p=="<http://www.w3.org/2002/07/owl#sameAs>"}.map{case(s,p,o)=>(s,o)}
+val sameAs = triples0.filter(x=>x._2=="<http://www.w3.org/2002/07/owl#sameAs>").map{case(s,p,o)=>(s,o)}
 
 // create an RDD containing individuals involved in sameAs triples
 val sameAsInd = sameAs.flatMap{case(s,o)=>Array(s,o)}.distinct
+
+// create an RDD made of statements where the property is not owl:sameAs
+val nonSameAs = triples0.filter{case(s,p,o)=>p!="<http://www.w3.org/2002/07/owl#sameAs>"}.map{case(s,p,o)=>(s,o.replace('\"',' ').trim)}
+
+// create an RDD containing nonSameAs individuals
+val nonSameAsInd = nonSameAs.flatMap{case(s,o)=>Array(s,o)}.distinct.subtract(sameAsInd)
+
+// Create non SameAs Dictionary
+val nonSameAsDictionary = nonSameAsInd.zipWithUniqueId.map{case(member, id)=> ((id,0),member)}.toDS
+
+val nonSameAsLimit = nonSameAsDictionary.count * 2
 
 // provide a unique Long id to all sameAs individuals
 val sameAsIndId = sameAsInd.zipWithUniqueId
@@ -32,38 +44,14 @@ val sameAsGraph = Graph(sameAsIndId.map{case(uri,id)=>(id,uri)}, sameAsEdges)
 // Compute connected components of the graph
 val connectedComponents = sameAsGraph.connectedComponents
 
-// create an RDD containing the ids of connected components
-val sameAsGroup = connectedComponents.vertices.map{x=> x._2}.distinct
-
-// create an RDD made of statements where the property is not owl:sameAs
-val nonSameAs = triples0.filter{case(s,p,o)=>p!="<http://www.w3.org/2002/07/owl#sameAs>"}.map{case(s,p,o)=>(s,o.replace('\"',' ').trim)}
-
-// create an RDD containing nonSameAs individuals
-val nonSameAsInd = nonSameAs.flatMap{case(s,o)=>Array(s,o)}.distinct.subtract(sameAsInd)
-
-// number of bits required for the encoding of sameAs individuals
-val nonSameAsBit = (Math.log(nonSameAsInd.count*2)/Math.log(2)).ceil
-
-val nonSameAsDictionary = nonSameAsInd.zipWithUniqueId
-
-// number of bits required for the encoding of sameAs individuals
-val sameAsBit = (Math.log(sameAsGroup.count*2)/Math.log(2)).ceil
-
-///////////////////////////////////////////
-// Encode sameAsIndividuals
-
 // create an RDD containing the connected component id and an array of all URI in that individual cluster
 val sameAsURIConnectedComp = sameAsIndId.map(x=>(x._2,x._1)).join(connectedComponents.vertices.map(x=>x)).map{case(k,(uri,cid))=>(cid,uri)}.groupByKey
 
-// compute max number of members in a sameAsGroup
-val maxGroupSize = sameAsURIConnectedComp.map(x=> (x._2.size)).reduce(Math.max)
-
-// compute number of bits required for the largest sameAs group
-val maxGroupSizeBit = (Math.log(maxGroupSize)/math.log(2)).ceil
+val temp = sameAsURIConnectedComp.zipWithIndex.map{case((ccID,entities), id)=>(id+nonSameAsLimit, entities)}
 
 def zipId(map : Iterable[String] ) : List[(Long,String)] = {
   var res = ListBuffer[(Long,String)]()
-  var cumul : Long = 1;
+  var cumul : Long = 0;
   var i = map.iterator
   while (i.hasNext) {
     res.append((cumul,i.next))
@@ -72,25 +60,11 @@ def zipId(map : Iterable[String] ) : List[(Long,String)] = {
   return res.toList
 }
 
-// Signature : connectedComponent, sameAsId, sameAsLabel
-val sameAsDictionaryTemp = sameAsURIConnectedComp.map{case(id, l)=> (id, zipId(l))}.flatMap{case(cid, list) => list.map{case(id, u)=> (cid.toLong,id,u)}}
+val sameAsDictionary = temp.map{case(id, l)=> (id, zipId(l))}.flatMap{case(cid, list) => list.map{case(id, u)=> ((cid.toLong,id),u)}}
 
-def dicoRDD(saBit : Long, nonsaBit: Long, rdd : RDD[(Long, Long, String)]) : RDD[(Long, String)] = {
-    return rdd.map{case(cid,id,uri) => (1<<(saBit+nonsaBit) | (cid<<nonsaBit) + id, uri)}
-}
-
-// compute the size required for right most part for the sameAs groups
-val rightSizeBit = Math.max(maxGroupSizeBit,nonSameAsBit.toLong)
-
-// compute de sameAsDictionary
-val sameAsDictionary = dicoRDD(sameAsBit.toLong, rightSizeBit.toLong, sameAsDictionaryTemp)
-
-// Create metadata RDD
-val metadata = sc.parallelize(Array(("saGroupBits "+sameAsBit.toLong),("saLocalBits "+rightSizeBit.toLong)))
+val metadata = sc.parallelize(Array(nonSameAsLimit))
 
 // store dictionaries
 nonSameAsDictionary.map(x=> x._2+" "+x._1).saveAsTextFile(directory+"/dct/nonSameAs.dct")
 sameAsDictionary.map(x=>x._1+" "+x._2).saveAsTextFile(directory+"/dct/sameAs.dct")
 metadata.saveAsTextFile(directory+"/dct/metadata")
-
-
